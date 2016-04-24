9c9d126135b006ac76d6860c3f77e9325d825853
root <root@master.openshift.com>
2016-04-24 18:51:49 -0300

    Adjust the Status scripting

--- restart-openshift.sh
+++ restart-openshift.sh
@@ -1,4 +1,4 @@
-echo ">>> Restart Node1:atomic-openshift-node"
-ssh node1.openshift.com "systemctl restart docker; systemctl restart atomic-openshift-node"
 echo ">>> Restart atomic-openshift-master"
 systemctl restart docker; systemctl restart atomic-openshift-node; systemctl restart atomic-openshift-master
+echo ">>> Restart Node1:atomic-openshift-node"
+ssh node1.openshift.com "systemctl restart docker; systemctl restart atomic-openshift-node"
