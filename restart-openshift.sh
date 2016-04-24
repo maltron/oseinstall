2587c7e9c99a6fc9e41157e8755876bd9472d28f
root <root@master.openshift.com>
2016-04-24 10:05:58 -0300

    Added a create-pv.sh scripting

--- restart-openshift.sh
+++ restart-openshift.sh
@@ -1,10 +1,4 @@
-echo ">>> Restart infranode:atomic-openshift-node"
-ssh infranode.openshift.com "systemctl restart docker; systemctl restart atomic-openshift-node"
-echo ">>> Restart charlie:atomic-openshift-node"
-ssh nodecharlie.openshift.com "systemctl restart docker; systemctl restart atomic-openshift-node"
-echo ">>> Restart bravo:atomic-openshift-node"
-ssh nodebravo.openshift.com "systemctl restart docker; systemctl restart atomic-openshift-node"
-echo ">>> Restart alpha:atomic-openshift-node"
-ssh nodealpha.openshift.com "systemctl restart docker; systemctl restart atomic-openshift-node"
+echo ">>> Restart Node1:atomic-openshift-node"
+ssh node1.openshift.com "systemctl restart docker; systemctl restart atomic-openshift-node"
 echo ">>> Restart atomic-openshift-master"
 systemctl restart docker; systemctl restart atomic-openshift-node; systemctl restart atomic-openshift-master
