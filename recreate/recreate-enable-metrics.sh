37a60b0bf470c4dddabfc78d507a9f2cb9e088f1
root <root@master.workshop.com>
2016-06-09 17:39:01 -0400

    Modified metrics for a workshop.com domain

--- recreate/recreate-enable-metrics.sh
+++ recreate/recreate-enable-metrics.sh
@@ -10,5 +10,4 @@ API
 oadm policy add-role-to-user edit system:serviceaccount:openshift-infra:metrics-deployer
 oadm policy add-cluster-role-to-user cluster-reader system:serviceaccount:openshift-infra:heapster
 oc secrets new metrics-deployer nothing=/dev/null
-#oc process -f metrics.yaml -v HAWKULAR_METRICS_HOSTNAME=hawkular-metrics.cloudapps.openshift.com,USE_PERSISTENT_STORAGE=false,IMAGE_PREFIX=openshift/origin-,IMAGE_VERSION=latest | oc create -n openshift-infra -f -
-oc process -f metrics.yaml -v HAWKULAR_METRICS_HOSTNAME=hawkular-metrics.cloudapps.openshift.com,USE_PERSISTENT_STORAGE=false | oc create -n openshift-infra -f -
+oc process -f metrics.yaml -v HAWKULAR_METRICS_HOSTNAME=hawkular-metrics.workshop.openshift.com,USE_PERSISTENT_STORAGE=false | oc create -n openshift-infra -f -
