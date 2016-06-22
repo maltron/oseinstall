3159a01081cec083630eb5322c6caca677b05006
root <root@master.example.com>
2016-06-22 18:41:04 -0400

    Changed logging

--- recreate/remove-enable-metrics.sh
+++ recreate/remove-enable-metrics.sh
@@ -1,12 +1,5 @@
-for i in $(oc get secret | egrep "(hawkular|heapster|metrics)" | awk '{ print $1 }'); do
-    oc delete secret $i
-  done
-oc delete rc hawkular-metrics heapster hawkular-cassandra-1
-oc delete svc hawkular-cassandra hawkular-cassandra-nodes hawkular-metrics heapster
-oc delete route hawkular-metrics
-oc delete pod --all
-oc delete sa cassandra hawkular heapster metrics-deployer
-oc delete template hawkular-cassandra-node-emptydir hawkular-cassandra-node-pv \
-  hawkular-cassandra-services \
-  hawkular-heapster hawkular-metrics \
-  hawkular-support
+oc delete all --selector="metrics-infra" --namespace openshift-infra
+oc delete sa --selector="metrics-infra" --namespace openshift-infra
+oc delete templates --selector="metrics-infra" --namespace openshift-infra
+oc delete secrets --selector="metrics-infra" --namespace openshift-infra
+oc delete pvc --selector="metrics-infra" --namespace openshift-infra
