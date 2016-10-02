db31f4a6214a75451773c2585664a5caf6abdccf
root <root@openshift.example.com>
2016-10-01 22:07:37 -0400

    Added a Service Account for Metrics

--- recreate/recreate-enable-metrics.sh
+++ recreate/recreate-enable-metrics.sh
@@ -1,4 +1,12 @@
 oc project openshift-infra
+oc create -n openshift-infra -f - <<API
+apiVersion: v1
+kind: ServiceAccount
+metadata:
+  name: metrics-deployer
+secrets:
+- name: metrics-deployer
+API
 oadm policy add-role-to-user edit system:serviceaccount:openshift-infra:metrics-deployer
 oadm policy add-cluster-role-to-user cluster-reader system:serviceaccount:openshift-infra:heapster
 oc secrets new metrics-deployer nothing=/dev/null -n openshift-infra
