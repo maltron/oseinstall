9fb38c85402a22b819db98d52c4a82e39baa4142
root <root@example-master.example.com>
2017-03-19 16:07:26 -0400

    Adjust for OpenShift 3.4

--- recreate/recreate-enable-metrics.sh
+++ recreate/recreate-enable-metrics.sh
@@ -7,9 +7,11 @@ metadata:
 secrets:
 - name: metrics-deployer
 API
+oadm policy add-role-to-user view system:serviceaccount:openshift-infra:hawkular -n openshift-infra
 oadm policy add-role-to-user edit system:serviceaccount:openshift-infra:metrics-deployer
 oadm policy add-cluster-role-to-user cluster-reader system:serviceaccount:openshift-infra:heapster
 oc secrets new metrics-deployer nothing=/dev/null -n openshift-infra
-oc new-app -f /usr/share/openshift/examples/infrastructure-templates/enterprise/metrics-deployer.yaml \
+oc new-app -f /usr/share/ansible/openshift-ansible/roles/openshift_hosted_templates/files/v1.4/enterprise/metrics-deployer.yaml \
+   --as=system:serviceaccount:openshift-infra:metrics-deployer \
    -p HAWKULAR_METRICS_HOSTNAME=hawkular-metrics.cloudapps.example.com \
    -p USE_PERSISTENT_STORAGE=false -n openshift-infra
